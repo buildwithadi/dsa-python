5d1fb37c9d7b053d64982257ed4498541a3073de
buildwithadi <adityarawat.online@gmail.com>
2026-06-25 23:03:22 +0530

    2 - Leaders in an array

---
+++ arrays/code2.py
@@ -0,0 +1,50 @@
+# Leaders in an array
+
+# Given an array arr[] of size n, the task is to find all the Leaders in the array. An element is a Leader if it is greater than or equal to all the elements to its right side.
+
+# Note: The rightmost element is always a leader.
+
+# Examples:
+
+# Input: arr[] = [16, 17, 4, 3, 5, 2]
+# Output: [17 5 2]
+# Explanation: 17 is greater than all the elements to its right i.e., [4, 3, 5, 2], therefore 17 is a leader. 5 is greater than all the elements to its right i.e., [2], therefore 5 is a leader. 2 has no element to its right, therefore 2 is a leader.
+
+# Input: arr[] = [1, 2, 3, 4, 5, 2]
+# Output: [5 2]
+# Explanation: 5 is greater than all the elements to its right i.e., [2], therefore 5 is a leader. 2 has no element to its right, therefore 2 is a leader.
+
+
+# Brute Force Method:
+# class Solution:
+#     def leaders(self, arr):
+#         res = list()
+#         n = len(arr)
+        
+#         for i in range(0,n-1):
+#             flag = True
+#             for j in range(i,n):
+#                 if arr[j] > arr[i]:
+#                     flag = False
+#                     break
+#             if flag:
+#                 res.append(arr[i])
+        
+#         res.append(arr[n-1])
+        
+#         return res
+
+# Using Suffix Maximum
+class Solution:
+    def leaders(self, arr):
+        res = list()
+        n = len(arr)
+        
+        max = -1;
+        for i in range(n-1,-1,-1):
+            if arr[i] >= max:
+                max = arr[i]
+                res.append(arr[i])
+        
+        return res[::-1]
+            
