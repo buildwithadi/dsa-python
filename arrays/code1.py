dc270e5d70698a22e46aa01c7728708d236f87a4
buildwithadi <adityarawat.online@gmail.com>
2026-06-25 22:26:56 +0530

    1 - Alternate elements of an array

--- arrays/code1.py
+++ arrays/code1.py
@@ -7,6 +7,14 @@
 # Explanation: Print the first element (10), skip the second element (20), print the third element (30), skip the fourth element(40) and print the fifth element(50).
 
 # Input: arr[] = [-5, 1, 4, 2, 12]
-# Output: -5 4 12/
+# Output: -5 4 12
 
-
+class Solution:
+    def getAlternates(self, arr):
+        count = 1
+        l = list()
+        for i in arr:
+            if count % 2 != 0:
+                l.append(i)
+            count += 1
+        return l
